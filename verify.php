a6a4f85c3929062aac74e238ccc3019a6a4f4364
ttnhan2407 <heinekencn@gmail.com>
2021-10-22 16:47:32 +0700

    updataverify

--- verify.php
+++ verify.php
@@ -2,20 +2,20 @@
 	$email = $_POST['inputEmail'];
 	$pswd = $_POST['inputPasswd'];
 
-	$conn = mysqli_connect("localhost", "root", "", "www_project");
+	$conn = pg_connect("postgres://jeefjvaujfztkq:c5b9532ccfdb36b51c68712438db56a78baf3c52aa1b44863b5145c3bce44eea@ec2-3-228-78-248.compute-1.amazonaws.com:5432/dc5hr2p60ti82h");
 	if(!$conn){
 		echo "Cannot connecto to database " . mysqli_connect_error($conn);
 		exit;
 	}
 
 	$query = "SELECT username, password FROM admin";
-	$result = mysqli_query($conn, $query);
+	$result = pg_query($conn, $query);
 	if(!$result){
 		echo "Empty!";
 		exit;
 	}
 
-	while ($row = mysqli_fetch_assoc($result)){
+	while ($row = pg_fetch_assoc($result)){
 		if($email == $row['username'] && $pswd == $row['password']){
 			echo "Welcome admin! Long time no see";
 			break;
